11de8eda7cf110e0bc8880eb6150f023b7707dfc
jhpark <mugenjh@gmail.com>
2020-02-26 22:03:15 +0900

    1108. Defanging an IP Address

---
+++ src/easy/P1108.java
@@ -0,0 +1,22 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class P1108 {
+
+    public String defangIPaddr(final String address) {
+        return address.replace(".", "[.]");
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("1[.]1[.]1[.]1", defangIPaddr("1.1.1.1"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("255[.]100[.]50[.]0", defangIPaddr("255.100.50.0"));
+    }
+}

4e5b4b79691f9cc61e92778dfbdea554318f05e7
jhpark <mugenjh@gmail.com>
2020-03-07 23:23:40 +0900

    Plus One

--- src/explore/arrayandstring/PlusOne.java
+++ src/explore/arrayandstring/PlusOne.java
@@ -8,12 +8,11 @@ public class PlusOne {
 
     public int[] plusOne(final int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] == 9) {
-                digits[i] = 0;
-                continue;
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
             }
-            digits[i]++;
-            return digits;
+            digits[i] = 0;
         }
         int[] result = new int[digits.length + 1];
         result[0] = 1;

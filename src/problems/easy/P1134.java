a279db1c26f45ad8c9e086917e6cc73b2bb82fe6
jhpark <mugenjh@gmail.com>
2020-03-17 20:00:23 +0900

    1134. Armstrong Number

---
+++ src/problems/easy/P1134.java
@@ -0,0 +1,31 @@
+package problems.easy;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 1134. Armstrong Number
+public class P1134 {
+
+    public boolean isArmstrong(final int n) {
+        int temp = n, sum = 0;
+        int length = (int) Math.log10(n) + 1;
+        while (temp > 0) {
+            sum += Math.pow(temp % 10, length);
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+
+    @Test
+    public void test1() {
+        assertThat(isArmstrong(153)).isTrue();
+    }
+
+    @Test
+    public void test2() {
+        assertThat(isArmstrong(123)).isFalse();
+    }
+
+}

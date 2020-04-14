d9ee95131cc4fc5a39b76f0fbed53604b1d493ad
jhpark <mugenjh@gmail.com>
2020-04-14 23:06:35 +0900

    Perform String Shifts

--- src/explore/challenge30day/week2/PerformStringShifts.java
+++ src/explore/challenge30day/week2/PerformStringShifts.java
@@ -1,8 +1,5 @@
 package explore.challenge30day.week2;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,21 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PerformStringShifts {
 
     public String stringShift(String s, int[][] shift) {
-        LinkedList<String> list = new LinkedList<>(Arrays.asList(s.split("")));
         int shiftCount = 0;
         for (int[] input : shift) {
             shiftCount += input[0] == 0 ? input[1] : input[1] * -1;
         }
-        if (shiftCount > 0) {
-            for (int i = 0; i < shiftCount; i++) {
-                list.addLast(list.removeFirst());
-            }
-        } else {
-            for (int i = shiftCount; i < 0; i++) {
-                list.addFirst(list.removeLast());
-            }
-        }
-        return String.join("", list);
+        int shiftIndex = shiftCount % s.length();
+        shiftIndex += shiftCount < 0 ? s.length() : 0;
+        return s.substring(shiftIndex) + s.substring(0, shiftIndex);
     }
 
     @Test
@@ -37,6 +26,24 @@ public class PerformStringShifts {
     public void test2() {
         int[][] shift = new int[][]{{1, 1}, {1, 1}, {0, 2}, {1, 3}};
         assertThat(stringShift("abcdefg", shift)).isEqualTo("efgabcd");
+    }
+
+    @Test
+    public void test3() {
+        int[][] shift = new int[][]{{1, 1}, {1, 1}, {0, 2}, {0, 2}};
+        assertThat(stringShift("abcde", shift)).isEqualTo("cdeab");
+    }
+
+    @Test
+    public void test4() {
+        int[][] shift = new int[][]{{1, 8}, {1, 4}, {1, 3}, {1, 6}, {0, 6}, {1, 4}, {0, 2}, {0, 1}};
+        assertThat(stringShift("yisxjwry", shift)).isEqualTo("yisxjwry");
+    }
+
+    @Test
+    public void test5() {
+        int[][] shift = new int[][]{{1, 4}, {0, 5}, {0, 4}, {1, 1}, {1, 5}};
+        assertThat(stringShift("mecsk", shift)).isEqualTo("kmecs");
     }
 
 }

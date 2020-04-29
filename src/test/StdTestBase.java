1a3c6295179003f172f7543cbb7a73520af64e01
jhpark <mugenjh@gmail.com>
2020-04-29 21:38:45 +0900

    Standard In/Output Test Base

---
+++ src/test/StdTestBase.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class StdTestBase {
+
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        System.setIn(System.in);
+    }
+
+    protected void setIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    protected ByteArrayOutputStream getOut() {
+        return outContent;
+    }
+
+    protected String getOutStr() {
+        return outContent.toString();
+    }
+
+}
